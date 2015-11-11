24e56d08f267f1f23c67a0c6a216bdbcb6656578
michael <michael@cormorant>
2015-11-10 19:49:47 -0500

    cleaning up

--- app.js
+++ app.js
@@ -5,19 +5,13 @@
 // Other useful resources:
 // https://github.com/mapbox/node-sqlite3/wiki/API
 
-// Notes:
-// -maybe have unique ID for each item on the list.  Otherwise
-//  there is the risk of confusion over having the same item twice.
-//  Use this instead of the "Date" field?
-
-//require( './db');
 
 var express        = require( 'express' );
 var http           = require( 'http' );
 var path           = require( 'path' );
 var engine         = require( 'ejs-locals' );
 var app    = express();
-var routes = require( './routes' );
+//var routes = require( './routes' );
 
 app.set( 'port', process.env.PORT || 3001 );
 app.engine( 'ejs', engine );
@@ -33,14 +27,7 @@ app.use( express.methodOverride() );
 app.use( app.router );
 app.use( express.static( path.join( __dirname, 'public' )) );
 
-// routes
-//app.get( '/', routes.index );
-app.post( '/create', routes.create );
-app.get( '/destroy/:id', routes.destroy );
-app.get( '/edit/:id', routes.edit );
-app.post( '/update/:id', routes.update );
-
-app.get( '/', routes.index );
+app.get( '/', function(req, res) { res.render( 'layout')});
 
 http.createServer( app ).listen( app.get( 'port' ), function(){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
--- db.js
+++
@@ -1,5 +0,0 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('groceryList.db');
-
-db.run('CREATE TABLE groceries (name VARCHAR(20), updated_at DATE)');
-
